60cb9be912e8c18addd79e888874b63c82c7ec0e
Akshath Pandey <akshathpandey4266@gmail.com>
2022-08-01 19:59:15 +0530

    more info about the text and tolower button

--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -9,22 +9,33 @@ export default function TextForm(props) {
         let upCase=text.toUpperCase();
         setText(upCase);
     }
+    const handleLowerCaseClick=()=>{
+        console.log("Lower case button was clicked...");
+        let loCase=text.toLowerCase();
+        setText(loCase);
+    }
     const handleOnChange=(event)=>{
         console.log("On change trigged...");
         setText(event.target.value);
     }
   return (
     <>
-
+    <div className="container">
     <h3>{props.heading}</h3>
     <form>
         <div className="form-group">
             
             <textarea className="form-control" value={text} onChange={handleOnChange} id="myBox" rows="8"></textarea>
         </div>
-        <button className="btn btn-primary my-4" onClick={handleUpperCaseClick}>Convert To UpperCase</button>
+        <button className="btn btn-primary my-4 mx-2" onClick={handleUpperCaseClick}>Convert To UpperCase</button>
+        <button className="btn btn-primary my-4 mx-2" onClick={handleLowerCaseClick}>Convert To LowerCase</button>
 
     </form>
+    </div>
+    <div className="container my-3">
+        <h2>Your Text Summary</h2>
+        <p>Words are {text.split(" ").length} letters are {text.length}</p>
+    </div>
     </>
   )
 }
